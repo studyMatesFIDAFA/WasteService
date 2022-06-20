40529ea997d60e6e53027bb41335e3ad4e87398b
fabioscaglia <fabioscagliarini99@gmail.com>
2022-06-20 15:33:01 +0200

    progetto iniziale

---
+++ demo0.pl
@@ -0,0 +1,7 @@
+%====================================================================================
+% demo0 description   
+%====================================================================================
+context(ctxdemo0, "localhost",  "TCP", "8095").
+ qactor( demo0, ctxdemo0, "it.unibo.demo0.Demo0").
+  qactor( perceiver, ctxdemo0, "it.unibo.perceiver.Perceiver").
+  qactor( sender, ctxdemo0, "it.unibo.sender.Sender").
