23dfae495affeec4ca1a34db0f6ee7575a5858f9
fabioscaglia <fabioscagliarini99@gmail.com>
2022-07-01 15:00:26 +0200

    analisi problema v2

--- sprint1/analisi_problema.pl
+++ sprint1/analisi_problema.pl
@@ -3,6 +3,6 @@
 %====================================================================================
 context(ctxanalisiproblema, "localhost",  "TCP", "8050").
  qactor( trolley, ctxanalisiproblema, "it.unibo.trolley.Trolley").
-  qactor( waste_service, ctxanalisiproblema, "it.unibo.waste_service.Waste_service").
+  qactor( wasteservice, ctxanalisiproblema, "it.unibo.wasteservice.Wasteservice").
   qactor( wastetruckmock, ctxanalisiproblema, "it.unibo.wastetruckmock.Wastetruckmock").
   qactor( wastetruckmock2, ctxanalisiproblema, "it.unibo.wastetruckmock2.Wastetruckmock2").
--- sprint1/analisi_requisiti.pl
+++
@@ -1,6 +0,0 @@
-%====================================================================================
-% analisi_requisiti description   
-%====================================================================================
-context(ctxanalisirequisiti, "localhost",  "TCP", "8050").
- qactor( waste_service, ctxanalisirequisiti, "it.unibo.waste_service.Waste_service").
-  qactor( waste_truck_mock, ctxanalisirequisiti, "it.unibo.waste_truck_mock.Waste_truck_mock").
