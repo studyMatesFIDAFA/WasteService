453cc0fff1305e5874e23f4b4293e469500f49da
Filippo Comastri <filippo.comastri2@studio.unibo.it>
2022-07-08 10:47:26 +0200

    pulizia generale pusssss

--- analisi_requisiti.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% analisi_requisiti description   
-%====================================================================================
-context(ctxanalisirequisiti, "localhost",  "TCP", "8080").
- qactor( trolley, ctxanalisirequisiti, "it.unibo.trolley.Trolley").
-  qactor( waste_service, ctxanalisirequisiti, "it.unibo.waste_service.Waste_service").
-  qactor( sonar, ctxanalisirequisiti, "it.unibo.sonar.Sonar").
-  qactor( led, ctxanalisirequisiti, "it.unibo.led.Led").
-  qactor( waste_truck_mock, ctxanalisirequisiti, "it.unibo.waste_truck_mock.Waste_truck_mock").
--- analisiproblema.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% analisiproblema description   
-%====================================================================================
-context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
-context(ctxanalisiproblema, "localhost",  "TCP", "8078").
- qactor( pathexec, ctxbasicrobot, "external").
-  qactor( trolley, ctxanalisiproblema, "it.unibo.trolley.Trolley").
-  qactor( wasteservice, ctxanalisiproblema, "it.unibo.wasteservice.Wasteservice").
