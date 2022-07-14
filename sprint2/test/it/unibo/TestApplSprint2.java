43fe340173ffeea15c006822be37ea065f8aabdf
DavideFiloni <filonidavide@gmail.com>
2022-07-14 14:42:00 +0200

    test sprint 2

--- sprint2/test/it/unibo/TestApplSprint2.java
+++ sprint2/test/it/unibo/TestApplSprint2.java
@@ -2,7 +2,6 @@ package it.unibo;
 
 import static org.junit.Assert.*;
 
-import it.unibo.ctxanalisiproblema.MainCtxanalisiproblemaKt;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapHandler;
 import org.eclipse.californium.core.CoapResponse;
@@ -17,18 +16,20 @@ import unibo.actor22comm.utils.CommUtils;
 
 
 public class TestApplSprint2 {
+
 	@Before
 	public void up() {
 	new Thread(){
 			public void run(){
-                MainCtxanalisiproblema.main();
+                it.unibo.ctxanalisiproblema.MainCtxanalisiproblemaKt.main();
 			}
 		}.start();
 		waitForApplStarted();
 	}
 
-	protected void waitForApplStarted(){
+	protected void waitForApplStarted() {
 		ActorBasic wasteservice = QakContext.Companion.getActor("wasteservice");
+
 		while( wasteservice == null ){
 			System.out.println("Attendo applicazione ... ");
 			CommUtils.delay(200);
@@ -45,19 +46,18 @@ public class TestApplSprint2 {
 	@Test
 	public void stopAndLedOn() throws Exception {
 		 CoapClient client = new CoapClient("coap://localhost:8078/ctxanalisiproblema/trolley");
+		 CoapClient clientLed = new CoapClient("coap://localhost:8080/ctxraspy/led");
 		 String stop = "msg(stop,dispatch,algise,wasteservice,stop(70),18)";
 		 try{
 			 ConnTcp connTcp   = new ConnTcp("localhost", 8078);
-			 ConnTcp connRaspy = new ConnTcp("127.0.0.1", 8080);
+			 Thread.sleep(1000);
 			 connTcp.forward(stop);
-			 String cmd = connRaspy.receiveMsg();
-			 assertTrue(cmd.contains("led,cmd(on),"));
 			 connTcp.close();
-			 connRaspy.close();
 		 }catch(Exception e) {
 			 System.out.println("testLoadok ERROR:" + e.getMessage());
 		 }
 
+		 Thread.sleep(2000);
 		 client.observe(new CoapHandler() {
 			 @Override
 			 public void onLoad(CoapResponse response){
@@ -70,7 +70,19 @@ public class TestApplSprint2 {
 				 System.out.println("ERRORE");
 			 }
 		 });
+		clientLed.observe(new CoapHandler() {
+			@Override
+			public void onLoad(CoapResponse response){
+				System.out.println("Response: "+response.getResponseText());
+				assertTrue(response.getResponseText().contains("ON"));
+			}
 
+			@Override
+			public void onError() {
+				System.out.println("ERRORE");
+			}
+		});
+		Thread.sleep(3000);
 	}
 
 	@Test
@@ -100,12 +112,14 @@ public class TestApplSprint2 {
 				System.out.println("ERRORE");
 			}
 		});
-
+		Thread.sleep(3000);
 	}
 
 	@Test
-	public void ledOff() throws Exception {
+	public void homeAndLedOff() throws Exception {
 		CoapClient client = new CoapClient("coap://localhost:8078/ctxanalisiproblema/trolley");
+		CoapClient clientLed = new CoapClient("coap://localhost:8080/ctxraspy/led");
+		Thread.sleep(2000);
 		client.observe(new CoapHandler() {
 			@Override
 			public void onLoad(CoapResponse response){
@@ -118,35 +132,49 @@ public class TestApplSprint2 {
 				System.out.println("ERRORE");
 			}
 		});
-		try{
-			ConnTcp connRaspy = new ConnTcp("127.0.0.1", 8080);
-			String cmd = connRaspy.receiveMsg();
-			assertTrue(cmd.contains("led,cmd(off),"));
-			connRaspy.close();
-		}catch(Exception e) {
-			System.out.println("testLoadok ERROR:" + e.getMessage());
-		}
+		clientLed.observe(new CoapHandler() {
+			@Override
+			public void onLoad(CoapResponse response){
+				System.out.println("Response: "+response.getResponseText());
+				assertTrue(response.getResponseText().contains("OFF"));
+			}
 
+			@Override
+			public void onError() {
+				System.out.println("ERRORE");
+			}
+		});
+		Thread.sleep(2000);
 	}
 
 	@Test
-	public void testLoadacceptAndBlink() {
+	public void testLoadacceptAndBlink() throws InterruptedException {
 		String truckRequestStr = "msg(load_req, request, algise,wasteservice,load_req(vetro,2),1)";
+		CoapClient clientLed = new CoapClient("coap://localhost:8080/ctxraspy/led");
 		try{
 			ConnTcp connTcp   = new ConnTcp("localhost", 8078);
-			ConnTcp connRaspy = new ConnTcp("127.0.0.1", 8080);
+
 			String answer     = connTcp.request(truckRequestStr);
 			System.out.println("Risposta=" + answer );
 			assertTrue(answer.contains("loadaccept"));
-			String cmd = connRaspy.receiveMsg();
-			assertTrue(cmd.contains("led,cmd(blink),"));
-			connRaspy.close();
 			connTcp.close();
 
 		}catch(Exception e){
 			System.out.println("testLoadok ERROR:" + e.getMessage());
 
 		}
+		clientLed.observe(new CoapHandler() {
+			@Override
+			public void onLoad(CoapResponse response){
+				System.out.println("Response: "+response.getResponseText());
+				assertTrue(response.getResponseText().contains("BLINK"));
+			}
 
+			@Override
+			public void onError() {
+				System.out.println("ERRORE");
+			}
+		});
+		Thread.sleep(3000);
 	}
 }
