dc9d8c842d36040605e92a6eb4e3b07599c0d1ec
DavideFiloni <filonidavide@gmail.com>
2022-07-18 11:46:42 +0200

    Sprint 3

---
+++ WasteServiceStatusGUI/src/main/java/unibo/WasteServiceStatusGUI/WasteServiceStatusGuiApplication.java
@@ -0,0 +1,13 @@
+package unibo.WasteServiceStatusGUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteServiceStatusGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteServiceStatusGuiApplication.class, args);
+	}
+
+}
---
+++ WasteServiceStatusGUI/src/test/java/unibo/WasteServiceStatusGUI/WasteServiceStatusGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package unibo.WasteServiceStatusGUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteServiceStatusGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
